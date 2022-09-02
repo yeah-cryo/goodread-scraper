eea8a1b561ca785d0f43840873afbb476a389f17
GunslingerGinger <47130442+GunslingerGinger@users.noreply.github.com>
2022-09-01 19:06:05 -0500

    moving neccesary asset

---
+++ node_modules/handsontable/dataMap/index.mjs
@@ -0,0 +1,5 @@
+import DataMap from "./dataMap.mjs";
+import MetaManager from "./metaManager/index.mjs";
+import metaSchemaFactory from "./metaManager/metaSchema.mjs";
+import { DynamicCellMetaMod } from "./metaManager/mods/dynamicCellMeta.mjs";
+export { DataMap, MetaManager, DynamicCellMetaMod, metaSchemaFactory };
